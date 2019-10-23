da9a9562cdc13f3aa22c2337ba088d53ba62757d
Carleigh Crockett <capleugh@gmail.com>
2019-10-23 09:17:52 -0600

    Create patron and exhibit classes and tests

---
+++ test/exhibit_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+
+class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new("Gems and Minerals", 0)
+    bob = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_initialize
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
+end
